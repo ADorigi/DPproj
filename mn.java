445ac4d761d95a0cb3bfb80bef76aadf71e60d87
Adorigi <gulegulzaradnan@gmail.com>
2019-02-18 23:17:23 +0530

    Number of clusters p

--- mn.java
+++ mn.java
@@ -2,13 +2,14 @@ package projfiles;
 
 import java.io.*;
 import java.util.*;
+import java.lang.Math;
 
 class mn{
     public static void main(String[] args) throws FileNotFoundException, IOException{
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T,k;
+        int T,k,p;
 
         List<String[]> dataset = new ArrayList<String[]>();
         dataset=new cls().readData();
@@ -19,7 +20,9 @@ class mn{
         System.out.print("Enter value of k:");
         k=Integer.parseInt(br.readLine());
 
-
-
+        System.out.print("Number of clusters are:");
+        p=(int)Math.floor(T/k);
+        System.out.println(p);
+        
     }
 }
